ada10fc33e307639d0f9cecd0f7502d8693f104e
Zastinian <zastinian@gmail.com>
2024-04-04 16:53:48 -0600

    feat: Added utils_buffer

--- Package/src/buffers.rs
+++ Package/src/buffers.rs
@@ -14,3 +14,18 @@ pub fn filter_buffer(api_key: String, filter_type: &str, params: String) -> Vec<
 
     response.bytes().expect("Failed to read response").to_vec()
 }
+
+pub fn utils_buffer(api_key: String, util_type: &str, params: String) -> Vec<u8> {
+    let url = format!(
+        "https://strangeapi.hostz.me/api/utils/{}?{}",
+        util_type, params
+    );
+    let client = Client::new();
+    let response = client
+        .get(&url)
+        .header("Authorization", format!("Bearer {}", api_key))
+        .send()
+        .expect("Failed to fetch image");
+
+    response.bytes().expect("Failed to read response").to_vec()
+}
