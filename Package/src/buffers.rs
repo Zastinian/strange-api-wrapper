43d6fa444f16641a0bca3b4a2113791e64dda992
Zastinian <zastinian@gmail.com>
2024-04-04 19:25:04 -0600

    feat: Added generators_buffer

--- Package/src/buffers.rs
+++ Package/src/buffers.rs
@@ -15,6 +15,21 @@ pub fn filter_buffer(api_key: String, filter_type: &str, params: String) -> Vec<
     response.bytes().expect("Failed to read response").to_vec()
 }
 
+pub fn generators_buffer(api_key: String, util_type: &str, params: String) -> Vec<u8> {
+    let url = format!(
+        "https://strangeapi.hostz.me/api/generators/{}?{}",
+        util_type, params
+    );
+    let client = Client::new();
+    let response = client
+        .get(&url)
+        .header("Authorization", format!("Bearer {}", api_key))
+        .send()
+        .expect("Failed to fetch image");
+
+    response.bytes().expect("Failed to read response").to_vec()
+}
+
 pub fn overlays_buffer(api_key: String, util_type: &str, params: String) -> Vec<u8> {
     let url = format!(
         "https://strangeapi.hostz.me/api/overlays/{}?{}",
